f154e9083ec894ea059fd521ff2fe22129dadc5a
jahanzeb011 <99148364+jahanzeb011@users.noreply.github.com>
2023-11-07 01:47:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,293 @@
+
+
+function toggleDropdown(){
+    var dropdown = document.getElementById("dropdown");
+     var plus1 = document.getElementById("plus1");
+     var dropdown1 = document.getElementById("dropdown1");
+     var plus2 = document.getElementById("plus2");
+     var dropdown2 = document.getElementById("dropdown2");
+     var plus3 = document.getElementById("plus3");
+     var dropdown3 = document.getElementById("dropdown3");
+     var plus4 = document.getElementById("plus4");
+     
+     if (dropdown1.style.display === "block") {
+       dropdown1.style.display = "none";
+       plus2.innerHTML = "+";
+     }
+   
+     if (dropdown2.style.display === "block") {
+       dropdown2.style.display = "none";
+       plus3.innerHTML = "+";
+     }
+   
+     if (dropdown3.style.display === "block") {
+       dropdown3.style.display = "none";
+       plus4.innerHTML = "+";
+     }
+   
+      if (dropdown4.style.display === "block") {
+       dropdown4.style.display = "none";
+       plus5.innerHTML = "+";
+     }
+   
+     if (dropdown5.style.display === "block") {
+       dropdown5.style.display = "none";
+       plus6.innerHTML = "+";
+     }
+     
+     if (dropdown.style.display === "block") {
+       dropdown.style.display = "none";
+       plus1.innerHTML = "+";
+     }
+       
+     else {
+       dropdown.style.display = "block";
+       plus1.innerHTML = "×";
+     }
+   }
+   
+   function toggleDropdown1(){
+    var dropdown1 = document.getElementById("dropdown1");
+     var plus2 = document.getElementById("plus2");
+      var dropdown = document.getElementById("dropdown");
+     var plus1 = document.getElementById("plus1");
+     var dropdown2 = document.getElementById("dropdown2");
+     var plus3 = document.getElementById("plus3");
+     var dropdown3 = document.getElementById("dropdown3");
+     var plus4 = document.getElementById("plus4");
+     
+     if (dropdown.style.display === "block") {
+       dropdown.style.display = "none";
+       plus1.innerHTML = "+";
+     }
+   
+     if (dropdown2.style.display === "block") {
+       dropdown2.style.display = "none";
+       plus3.innerHTML = "+";
+     }
+   
+     if (dropdown3.style.display === "block") {
+       dropdown3.style.display = "none";
+       plus4.innerHTML = "+";
+     }
+   
+      if (dropdown4.style.display === "block") {
+       dropdown4.style.display = "none";
+       plus5.innerHTML = "+";
+     }
+   
+     if (dropdown5.style.display === "block") {
+       dropdown5.style.display = "none";
+       plus6.innerHTML = "+";
+     }
+     
+     if (dropdown1.style.display === "block") {
+       dropdown1.style.display = "none";
+       plus2.innerHTML = "+";
+     }
+    
+     else {
+       dropdown1.style.display = "block";
+       plus2.innerHTML = "×";
+     }
+   }
+   
+   function toggleDropdown2(){
+     var dropdown1 = document.getElementById("dropdown1");
+     var plus2 = document.getElementById("plus2");
+      var dropdown = document.getElementById("dropdown");
+     var plus1 = document.getElementById("plus1");
+     var dropdown2 = document.getElementById("dropdown2");
+     var plus3 = document.getElementById("plus3");
+     var dropdown3 = document.getElementById("dropdown3");
+     var plus4 = document.getElementById("plus4");
+   
+     if (dropdown.style.display === "block") {
+       dropdown.style.display = "none";
+       plus1.innerHTML = "+";
+     }
+     
+     if (dropdown1.style.display === "block") {
+       dropdown1.style.display = "none";
+       plus2.innerHTML = "+";
+     }
+   
+     if (dropdown3.style.display === "block") {
+       dropdown3.style.display = "none";
+       plus4.innerHTML = "+";
+     }
+   
+      if (dropdown4.style.display === "block") {
+       dropdown4.style.display = "none";
+       plus5.innerHTML = "+";
+     }
+   
+     if (dropdown5.style.display === "block") {
+       dropdown5.style.display = "none";
+       plus6.innerHTML = "+";
+     }
+     
+       if (dropdown2.style.display === "block") {
+       dropdown2.style.display = "none";
+       plus3.innerHTML = "+";
+     }
+   
+     
+     else {
+       dropdown2.style.display = "block";
+       plus3.innerHTML = "×";
+     }
+   }
+   
+   
+   function toggleDropdown3(){
+     var dropdown1 = document.getElementById("dropdown1");
+     var plus2 = document.getElementById("plus2");
+      var dropdown = document.getElementById("dropdown");
+     var plus1 = document.getElementById("plus1");
+     var dropdown2 = document.getElementById("dropdown2");
+     var plus3 = document.getElementById("plus3");
+     var dropdown3 = document.getElementById("dropdown3");
+     var plus4 = document.getElementById("plus4");
+   
+     if (dropdown.style.display === "block") {
+       dropdown.style.display = "none";
+       plus1.innerHTML = "+";
+     }
+     
+     if (dropdown1.style.display === "block") {
+       dropdown1.style.display = "none";
+       plus2.innerHTML = "+";
+     }
+     
+       if (dropdown2.style.display === "block") {
+       dropdown2.style.display = "none";
+       plus3.innerHTML = "+";
+     }
+   
+      if (dropdown4.style.display === "block") {
+       dropdown4.style.display = "none";
+       plus5.innerHTML = "+";
+     }
+   
+     if (dropdown5.style.display === "block") {
+       dropdown5.style.display = "none";
+       plus6.innerHTML = "+";
+     }
+   
+          if (dropdown3.style.display === "block") {
+       dropdown3.style.display = "none";
+       plus4.innerHTML = "+";
+     }
+   
+     
+     else {
+       dropdown3.style.display = "block";
+       plus4.innerHTML = "×";
+     }
+   }
+   
+   
+   function toggleDropdown4(){
+     var dropdown1 = document.getElementById("dropdown1");
+     var plus2 = document.getElementById("plus2");
+      var dropdown = document.getElementById("dropdown");
+     var plus1 = document.getElementById("plus1");
+     var dropdown2 = document.getElementById("dropdown2");
+     var plus3 = document.getElementById("plus3");
+     var dropdown3 = document.getElementById("dropdown3");
+     var plus4 = document.getElementById("plus4");
+     var dropdown4 = document.getElementById("dropdown4");
+     var plus5 = document.getElementById("plus5");
+   
+     if (dropdown.style.display === "block") {
+       dropdown.style.display = "none";
+       plus1.innerHTML = "+";
+     }
+     
+     if (dropdown1.style.display === "block") {
+       dropdown1.style.display = "none";
+       plus2.innerHTML = "+";
+     }
+     
+       if (dropdown2.style.display === "block") {
+       dropdown2.style.display = "none";
+       plus3.innerHTML = "+";
+     }
+   
+          if (dropdown3.style.display === "block") {
+       dropdown3.style.display = "none";
+       plus4.innerHTML = "+";
+     }
+   
+     if (dropdown5.style.display === "block") {
+       dropdown5.style.display = "none";
+       plus6.innerHTML = "+";
+     }
+   
+       if (dropdown4.style.display === "block") {
+       dropdown4.style.display = "none";
+       plus5.innerHTML = "+";
+     }
+   
+     else {
+       dropdown4.style.display = "block";
+       plus5.innerHTML = "×";
+     }
+   }
+   
+   
+   function toggleDropdown5(){
+     var dropdown1 = document.getElementById("dropdown1");
+     var plus2 = document.getElementById("plus2");
+      var dropdown = document.getElementById("dropdown");
+     var plus1 = document.getElementById("plus1");
+     var dropdown2 = document.getElementById("dropdown2");
+     var plus3 = document.getElementById("plus3");
+     var dropdown3 = document.getElementById("dropdown3");
+     var plus4 = document.getElementById("plus4");
+     var dropdown4 = document.getElementById("dropdown4");
+     var plus5 = document.getElementById("plus5");
+     var dropdown5 = document.getElementById("dropdown5");
+     var plus6 = document.getElementById("plus6");
+   
+     if (dropdown.style.display === "block") {
+       dropdown.style.display = "none";
+       plus1.innerHTML = "+";
+     }
+     
+     if (dropdown1.style.display === "block") {
+       dropdown1.style.display = "none";
+       plus2.innerHTML = "+";
+     }
+     
+       if (dropdown2.style.display === "block") {
+       dropdown2.style.display = "none";
+       plus3.innerHTML = "+";
+     }
+   
+          if (dropdown3.style.display === "block") {
+       dropdown3.style.display = "none";
+       plus4.innerHTML = "+";
+     }
+   
+       if (dropdown4.style.display === "block") {
+       dropdown4.style.display = "none";
+       plus5.innerHTML = "+";
+     }
+   
+         if (dropdown5.style.display === "block") {
+       dropdown5.style.display = "none";
+       plus6.innerHTML = "+";
+     }
+   
+     else {
+       dropdown5.style.display = "block";
+       plus6.innerHTML = "×";
+     }
+   }
+
+   // Go to the home page when the button is clicked
+document.getElementById('goToHomePage').addEventListener('click', () => {
+    window.location.href = 'home.html'; // Replace 'home.html' with your home page URL
+});
